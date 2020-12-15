54c0ecc1ae82eb0f4648e72289627a4606389a4d
Azad <azadjohari@gmail.com>
2020-12-15 20:29:57 +0800

    2029

---
+++ week06/scripts/fetch.js
@@ -0,0 +1,9 @@
+function GetBookings() {
+    let url = 'https://api.sheety.co/b9b23bacbce0fa05289abc34d8cf52e6/bookingApp/bookings';
+    fetch(url)
+        .then((response) => response.json())
+        .then(json => {
+            // Do something with the data
+            console.log(json.bookings);
+        });
+}
