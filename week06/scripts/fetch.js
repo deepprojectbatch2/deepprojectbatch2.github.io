ea87cf3adf67bf90c5d0a6e9102dd26f38b8cb04
Azad <azadjohari@gmail.com>
2020-12-15 20:33:38 +0800

    2033

--- week06/scripts/fetch.js
+++ week06/scripts/fetch.js
@@ -7,3 +7,8 @@ function GetBookings() {
             console.log(json.bookings);
         });
 }
+
+let getBookingBtn = document.getElementById("getBooking");
+getBookingBtn.addEventListener("click", function () {
+    GetBookings();
+});
